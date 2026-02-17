fe0efd2bb11b439d6cb1d5a9df676b5deb9621a3
Akkarin <74106482+Akk4r1n@users.noreply.github.com>
2026-02-17 19:47:28 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/symbol-attribute/',
   plugins: [react(), tailwindcss()],
   server: {
     fs: {
